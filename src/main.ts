a6b828202fc8cf7c4247889353a04680fc9ff762
UkanaBastreet <s.ppp@bk.ru>
2025-08-01 00:28:30 +0300

    add config to swagger (test)

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,18 @@ async function bootstrap() {
   const swaggerConfig = new DocumentBuilder().setTitle('BlindType Api').build();
   const documentFactory = () =>
     SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api', app, documentFactory, {
+    customSiteTitle: 'BlindType API',
+    customCssUrl:
+      'https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui.css',
+    customJs: [
+      'https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui-bundle.js',
+      'https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui-standalone-preset.js',
+    ],
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(APPLICATION_PORT, () =>
     console.log('App running at port: ' + APPLICATION_PORT),
